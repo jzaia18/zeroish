08520c5e129eade53753793e20c627a0e2773f69
adeeburrahman <57969730+adeeburrahman@users.noreply.github.com>
2018-01-23 00:33:12 -0500

    updated profile/leaderboard

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, session, request, flash
 import os, sqlite3, random
-from utils import auth, user
+from utils import auth, score, user
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
@@ -54,27 +54,57 @@ def tetris():
 
 @app.route("/profile")
 def profile():
-    if 'username' in session:
-        return render_template("profile.html", user=session['username'], image=user.get_avatar(session['username']), logged=True)
+    current_user = False
+    if 'user' in request.args:
+        username = request.args['user']
+        if 'username' in session:
+            if username == session['username']:
+                current_user = True
+    elif 'username' in session:
+        username = session['username']
+        current_user = True
     else:
         return redirect(url_for ('login') )
+    return render_template("profile.html", user=username, image=user.get_avatar(username),
+                           latest=score.get_scores(username), highscore=score.get_highscore(username),
+                           average=score.get_average(username), numplayed=score.get_numplayed(username),
+                           logged=True, current_user=current_user)
 
-@app.route("/highscores")
-def highscores():
+@app.route("/leaderboard")
+def leaderboard():
     if 'username' in session:
-        return render_template("highscores.html", logged=True)
+        logged = True
     else:
-        return render_template("highscores.html")
+        logged = False
+    return render_template("leaderboard.html", logged=logged,
+                           highscores = score.get_highscores(),
+                           averages = score.get_averages())
 
 @app.route("/update", methods=['POST'])
 def update():
     if 'username' in session:
-        return user.add_score(request.form['score'])
+        if 'score' in request.form:
+            return score.add_score(request.form['score'])
     return False
+
+
+@app.route("/avatar", methods=['POST'])
+def avatar():
+    if 'username' in session:
+        if 'image' in request.files:
+            image = request.files['image']
+            if image.content_type[:image.content_type.find('/')] == "image":
+                user.set_avatar('username', request.files['image'].stream.read().encode('base64').replace('\n',''))
+                flash("Avatar Sucessfully updated")
+            else:
+                flash("Upload Failed, provide an image file")
+    else:
+        flash("Upload failed")
+    return redirect(url_for ('profile') )
 
 if __name__ == "__main__":
     db = sqlite3.connect('data/database.db')
-    db.cursor().execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, hashed_pass TEXT, scores TEXT, average NUMERIC, highscore NUMERIC, numplayed NUMERIC, avatar BLOB);")
+    db.cursor().execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, hashed_pass TEXT, scores TEXT, average REAL, highscore NUMERIC, numplayed NUMERIC, avatar BLOB);")
     db.commit()
     db.close()
     app.debug = True
--- utils/auth.py
+++ utils/auth.py
@@ -6,14 +6,13 @@ def get_hashed_password(user, passw):
     return hashlib.sha256(passw + user).hexdigest()
 
 def register( user, passw):
+    if user_exists(user):
+        return False
     hashed = get_hashed_password(user, passw)
     db = sqlite3.connect(f)
-    user_info = db.cursor().execute('SELECT username FROM users WHERE username = "%s";' % ( user )).fetchall()
-    if len(user_info) != 0:
-        return False
     with open(avatar) as data:
         encoded = data.read().encode('base64').replace('\n','')
-    db.cursor().execute('INSERT INTO users VALUES ( "%s", "%s", "[]", 0, 0, 0, "%s" );' % (user, hashed, encoded) )
+    db.cursor().execute('INSERT INTO users VALUES ( "%s", "%s", "[]", 0.0, 0, 0, "%s" );' % (user, hashed, encoded) )
     db.commit()
     db.close()
     return True
@@ -28,21 +27,24 @@ def authenticate(user, passw):
     else:
         return False
 
-def get_avatar(user):
+def user_exists(user):
     db = sqlite3.connect(f)
-    user_info = db.cursor().execute('SELECT avatar FROM users WHERE username = "%s";' % ( user )).fetchall()
-    db.close()
-    return user_info[0][0]
+    user_info = db.cursor().execute('SELECT username FROM users WHERE username = "%s";' % ( user )).fetchall()
+    if len(user_info) > 0:
+        return True
+    return False
 
 if __name__ == '__main__':
     f = '../data/database.db'
     avatar = '../static/img/default.png'
     db = sqlite3.connect(f)
     c = db.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, hashed_pass TEXT, scores TEXT, average NUMERIC, highscore NUMERIC, numplayed NUMERIC, avatar BLOB);")
+    c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, hashed_pass TEXT, scores TEXT, average REAL, highscore NUMERIC, numplayed NUMERIC, avatar BLOB);")
     db.commit()
-    print 'Attempting to create account user, password:', register('user', 'password')
-    print 'Attempting to create account user, password:', register('user', 'password')
-    print 'Attempting to login with user, invalid:', authenticate('user', 'invalid')
-    print 'Attempting to login with user, password:', authenticate('user', 'password')
     db.close()
+    print 'Attempting to create account user, password:', register('user', 'user')
+    print 'Attempting to create account user, password:', register('user', 'user')
+    print 'Attempting to login with user, invalid:', authenticate('user', 'invalid')
+    print 'Attempting to login with user, password:', authenticate('user', 'user')
+    print 'Attempting to create account user, password:', register('test', 'test')
+    print 'Attempting to create account user, password:', register('w', 'w')
