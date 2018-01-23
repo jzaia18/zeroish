96ba180d5f89a4173debdd0320c0440711b9d63a
imahdi2000 <imahdi@stuy.edu>
2018-01-23 04:38:29 -0500

    main fxn of snek done, only pretty stuff left to do

--- static/js/snek.js
+++ static/js/snek.js
@@ -12,8 +12,8 @@ var snek_grow = 1;
 var tail_posx = [snek_posx];
 var tail_posy = [snek_posy];
 //position of fruit
-var apple_posx = 2;
-var apple_posy = 2;
+var apple_posx = random(1,length-1);
+var apple_posy = random(1,height-1);
 //is the game currently playing? Use for play/pause
 var playing = false;
 var game_over = false;
@@ -25,7 +25,7 @@ var inital;
 //necessary for length of snake
 var len = 0;
 //direction needs to be stored in var so that when update is made, direction can be updated
-var tempdir = dir;
+var temp_dir = dir;
 //keeps track of user score
 var score = 0;
 
@@ -36,6 +36,11 @@ function main(){
   //updater fxn
   inital = setInterval(loop, time)
 }
+
+var reset = function(){
+  location.reload();
+}
+
 
 //randomness needed for apples
 function random(min,max){
@@ -120,30 +125,33 @@ function draw_fruit(){
   }
 
 
-  window.addEventListener("keypress", function key(event){
+  window.addEventListener("keydown", function key(event){
       //W or up arrow set direction up
       var key = event.keyCode;
-      if(dir != 3 && (key == 119 || key == 87))
-          tempdir = 1;
+      if(dir != 3 && (key == 119 || key == 87 || key == 38))
+          temp_dir = 1;
       //S oe down arrow set direction down
-      else if(dir!= 1 && (key == 115 || key == 83))
-          tempdir = 3;
+      else if(dir!= 1 && (key == 115 || key == 83 || key == 40))
+          temp_dir = 3;
       //A or left arrow set direction left
-      else if(dir != 2 && (key == 97 || key == 65))
-          tempdir = 4;
+      else if(dir != 2 && (key == 97 || key == 65 || key == 37))
+          temp_dir = 4;
       //D or right arrow set direction right
-      else if(dir != 4 && (key == 100 || key == 68))
-          tempdir = 2;
+      else if(dir != 4 && (key == 100 || key == 68 || key == 39))
+          temp_dir = 2;
       //if spacebar is pressed pause game
       if(!playing)
           playing = true;
       else if(key == 32)
           playing = false;
+      if((playing == false && game_over == true) && key == 32){
+        reset();
+      }
   });
 
   function update(){
       //set the actual direction to the direction given by keypress
-      dir = tempdir;
+      dir = temp_dir;
       set_class(apple_posx, apple_posy, "apple");
       //makes non snake cells back to background cells
       set_class(tail_posx[len], tail_posy[len], "back");
@@ -160,16 +168,19 @@ function draw_fruit(){
           snek_posx++;
       //new cells of snek made
       for(var i = tail_posx.length-1; i >=0; i--){
-          if(snek_posx == tail_posx[i] && snek_posy == tail_posx[i]){
+        // this checks for collision by checking of snek lands on a snek cell
+          if((get_class(snek_posx, snek_posy) == "snek")){
               game_over = true;
               break;
           }
       }
-      set_class(snek_posx, snek_posy, "snek");
-
-      if(snek_posx == 0 || snek_posx == length-1 || snek_posy == 0 || snek_posy == height-1)
+      //snek collsion is being checked via borders
+      if(get_class(snek_posx, snek_posy) == "border")
           game_over = true;
-      else if(snek_posx == apple_posx && snek_posy == apple_posy){
+      //sets the new x,y postions of snek on the grid
+      set_class(snek_posx, snek_posy, "snek");
+      //if the snek eats apple, grow it and respawn apple
+      if(snek_posx == apple_posx && snek_posy == apple_posy){
           score += 1;
           draw_fruit();
           len += snek_grow;
